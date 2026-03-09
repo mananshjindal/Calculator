5644068ec3e46801d663704fd0a22b8acc84b390
Manansh Jindal <manansh4work@gmail.com>
2026-03-09 18:02:08 +0530

    Three numbers function, div by 0, num2 fix

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
+  if (b == 0) {
+    return "Error, Div by 0";
+  }
   return a / b;
 }
 let num1 = "";
 let num2 = "";
 let operand = "";
-function operation(num1, num2, operand) {
+function operation() {
   num1 = Number(num1);
   num2 = Number(num2);
   let result = "";
@@ -30,6 +33,9 @@ function operation(num1, num2, operand) {
     result = divide(num1, num2);
   }
   display.textContent = result;
+  num1 = result;
+  num2 = "";
+  operand = "";
 }
 const buttons = document.querySelector(".buttons");
 const display = document.querySelector(".display");
@@ -37,7 +43,10 @@ const display = document.querySelector(".display");
 buttons.addEventListener("click", (e) => assignVar(e));
 
 function assignVar(e) {
-  if (e.target.textContent >= 0 && e.target.textContent <= 9) {
+  if (!e.target.matches("button")) {
+    return;
+  }
+  if (!isNaN(e.target.textContent)) {
     if (operand == "") {
       num1 += e.target.textContent;
       display.textContent = num1;
@@ -46,6 +55,9 @@ function assignVar(e) {
       display.textContent = num2;
     }
   } else if (e.target.classList.contains("operator")) {
+    if (num2 != "") {
+      operation();
+    }
     operand = e.target.textContent;
   } else if (e.target.textContent == "C") {
     num1 = "";
@@ -53,6 +65,6 @@ function assignVar(e) {
     operand = "";
     display.textContent = "";
   } else if (e.target.textContent == "=") {
-    operation(num1, num2, operand);
+    operation();
   }
 }
