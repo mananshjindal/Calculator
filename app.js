d98e288328410309c03f1f5081d57b1cf5df6ce4
Manansh Jindal <manansh4work@gmail.com>
2026-03-09 16:16:51 +0530

    Added Basic Calculator functions and Clear.

--- app.js
+++ app.js
@@ -10,22 +10,49 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-let num1;
-let num2;
-let operand;
+let num1 = "";
+let num2 = "";
+let operand = "";
 function operation(num1, num2, operand) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  let result = "";
   if (operand == "+") {
-    add(num1, num2);
+    result = add(num1, num2);
   }
   if (operand == "-") {
-    subtract(num1, num2);
+    result = subtract(num1, num2);
   }
   if (operand == "*") {
-    multiply(num1, num2);
+    result = multiply(num1, num2);
   }
   if (operand == "/") {
-    divide(num1, num2);
+    result = divide(num1, num2);
+  }
+  display.textContent = result;
+}
+const buttons = document.querySelector(".buttons");
+const display = document.querySelector(".display");
+
+buttons.addEventListener("click", (e) => assignVar(e));
+
+function assignVar(e) {
+  if (e.target.textContent >= 0 && e.target.textContent <= 9) {
+    if (operand == "") {
+      num1 += e.target.textContent;
+      display.textContent = num1;
+    } else {
+      num2 += e.target.textContent;
+      display.textContent = num2;
+    }
+  } else if (e.target.classList.contains("operator")) {
+    operand = e.target.textContent;
+  } else if (e.target.textContent == "C") {
+    num1 = "";
+    num2 = "";
+    operand = "";
+    display.textContent = "";
+  } else if (e.target.textContent == "=") {
+    operation(num1, num2, operand);
   }
 }
-const buttons = document.querySelectorAll(".buttons");
-buttons.addEventListener("click", () => {});
